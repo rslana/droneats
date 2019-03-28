213ca5061204f3b7a4b1b736d477d72ea8155f88
Rafael S. L <rafael.souza.lana@gmail.com>
2019-03-28 14:43:53 -0300

    Adicionado arquivo de configuração e biblioteca de banco

---
+++ src/java/persistence/DatabaseLocator.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistence;
+
+//import com.mysql.jdbc.Connection;
+import config.Config;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author rslana
+ */
+public class DatabaseLocator {
+    private static DatabaseLocator instance = new DatabaseLocator();
+    public static DatabaseLocator getInstance(){
+        return instance;
+    }
+    
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost/droneats", Config.DB_NAME, Config.DB_PASS);
+    }
+}
