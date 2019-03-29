319fdcde1c1c3e1abbfa8e3842c1aea36651c85a
Rafael S. L <rafael.souza.lana@gmail.com>
2019-03-28 22:56:20 -0300

    Adicionado views: home, login, cadastro, cadastroCliente e cadastroRestaurante

---
+++ web/assets/js/cadastroRestaurante.js
@@ -0,0 +1,9 @@
+const mudarPassoCadastroRestaurante = (passo) => {
+  if (passo === 1) {
+    document.getElementById("passo1").style.display = "block";
+    document.getElementById("passo2").style.display = "none";
+  } else if (passo === 2) {
+    document.getElementById("passo1").style.display = "none";
+    document.getElementById("passo2").style.display = "block";
+  }
+}
